9b6cd71803500ef551b130b700b8ae59d71529c8
cracker <cracknlink@gmail.com>
2021-06-08 16:30:51 +0000

    jd_fq.js edited online with Bitbucket

--- jd_fq.js
+++ jd_fq.js
@@ -1,15 +1,26 @@
 /*
-author：tg@chenxing666
-分分币
-  Quantumult X:
-  [task_local]
-    38 0 * * * 
-  Loon:
-  [Script]
-    cron "38 0 * * *" 
-  Surge:
-    分分币 = type=cron,cronexp="38 0 * * *",w
-*/
+分分币，每日2毛～5毛
+可互助，助力码每日不变，只变日期
+活动入口：京东APP搜索领现金进入
+更新时间：2021-06-07
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+============Quantumultx===============
+[task_local]
+#分分币
+38 0 * * * https://bitbucket.org/jd-study/shylocks/src/master/jd_fq.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+
+================Loon==============
+[Script]
+cron "38 0 * * *" script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_fq.js,tag=签到领现金
+
+===============Surge=================
+分分币 = type=cron,cronexp="38 0 * * *",wake-system=1,timeout=3600,script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_fq.js
+
+============小火箭=========
+分分币 = type=cron,script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_fq.js, cronexpr="38 0 * * *", timeout=3600, enable=true
+ */
+
 const $ = new Env('分分币');
 
 const notify = $.isNode() ? require('./sendNotify') : '';
