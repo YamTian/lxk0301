94df9779d52eb06f31c13d4365d6e7413d2a838a
cracker <cracknlink@gmail.com>
2021-06-08 16:28:49 +0000

    jd_daojia_bean.js edited online with Bitbucket

--- jd_daojia_bean.js
+++ jd_daojia_bean.js
@@ -1,15 +1,25 @@
 /*
-author：tg@chenxing666
-鲜豆庄园
-  Quantumult X:
-  [task_local]
-    55 0 * * * 
-  Loon:
-  [Script]
-    cron "55 0 * * *" 
-  Surge:
-    鲜豆庄园 = type=cron,cronexp="55 0 * * *",w
-*/
+鲜豆庄园，每日2毛～5毛
+可互助，助力码每日不变，只变日期
+活动入口：京东APP搜索领现金进入
+更新时间：2021-06-07
+已支持IOS双京东账号,Node.js支持N个京东账号
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+============Quantumultx===============
+[task_local]
+#鲜豆庄园
+55 0 * * * https://bitbucket.org/jd-study/shylocks/src/master/jd_daojia_bean.js, tag=签到领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+
+================Loon==============
+[Script]
+cron "55 0 * * *" script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_daojia_bean.js,tag=签到领现金
+
+===============Surge=================
+鲜豆庄园 = type=cron,cronexp="55 0 * * *",wake-system=1,timeout=3600,script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_daojia_bean.js
+
+============小火箭=========
+鲜豆庄园 = type=cron,script-path=https://bitbucket.org/jd-study/shylocks/src/master/jd_daojia_bean.js, cronexpr="53 0 * * *", timeout=3600, enable=true
+ */
 
 const $ = new Env('鲜豆庄园');
 
